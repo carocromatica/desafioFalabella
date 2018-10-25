656d90ec97f3fc667f66d141c92bc7010e7f01fc
Evelyn Gálvez <evelyn.galvez.p@gmail.com>
2018-10-25 06:04:04 -0300

    intentando con puppeteer

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
-const puppeteer = require('puppeteer');
+
+  const index = function() {
+  const puppeteer = require('puppeteer');
+  const btn = document.getElementById('btn');
+  
+  btn.addEventListener('click', () => {
+    const title = document.getElementById('title').value;
+    return title;
+  })
+  
+  
+  (async () => {
+    try {
+      const url = 'https://listado.mercadolibre.cl/'+title+'#D[A:'+title+']';
+      const navegador = await puppeteer.launch()
+      const pagina = await navegador.newPage()
+      await pagina.goto(url)
+      const html = await pagina.content()
+      const informacion = await pagina.evaluate(()=> {
+        const productos = [...document.querySelectorAll('.item__title')
+      ].map((nodoProducto) => nodoProducto.innerText);
+  
+      const precios = [...document.querySelectorAll('.price__fraction')
+      ].map((nodoPrecio) => nodoPrecio.innerHTML);
+      return productos.map((producto, i) => (JSON.stringify({producto: producto, precio: precios[i]})))
+  
+      })
+  
+      console.log(informacion)
+      await navegador.close()
+    } catch (e) {
+  
+    }
+  })()
+}
+
+module.exports = index
 
 
-(async () => {
-  try {
-    const navegador = await puppeteer.launch()
-    const pagina = await navegador.newPage()
-    await pagina.goto('https://listado.mercadolibre.cl/samsung-galaxy-s8-64gb#D[A:samsung-galaxy-s8-64gb]')
-    const html = await pagina.content()
-    console.log(html);
-
-    let informacion = await pagina.evaluate(()=> {
-      const productos = [...document.querySelectorAll('.item__title')
-    ].map((nodoProducto) => nodoProducto.innerText);
-
-    const precios = [...document.querySelectorAll('.price__fraction')
-    ].map((nodoPrecio) => nodoPrecio.innerHTML);
-
-    return productos.map((producto, i) => ({producto: producto, precio: precios[i]}))
-
-    })
-
-    console.log(informacion)
-    await navegador.close()
-  } catch (e) {
-
-  }
-})()
